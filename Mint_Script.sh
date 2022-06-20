672be5f475f58bef07693f1fde9ce324987deeab
jenkinstudent <96473194+jenkinstudent@users.noreply.github.com>
2022-06-20 09:43:50 +0530

    Update
    Update

--- Mint_Script.sh
+++ Mint_Script.sh
@@ -5,6 +5,12 @@ sudo apt update
 sudo wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt install ./teamviewer_amd64.deb -y
 sudo apt update
-sudo apt-get autoremove -y 
+sudo apt-get autoremove -y
+# Boot Kernal disable ipv6
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash ipv6.disable=1"/g' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/g' /etc/default/grub
+sudo sudo update-grub
+#Disable IPV6 network interface
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -p	
